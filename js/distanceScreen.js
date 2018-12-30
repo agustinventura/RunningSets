e8b98575705a498486c7c93d66d9bf75c10d4a0a
Agustín Ventura <agustinventura@gmail.com>
2018-12-30 19:57:19 +0100

    finishes distance screen

--- js/distanceScreen.js
+++ js/distanceScreen.js
@@ -21,7 +21,62 @@ function setDistanceScreenListeners() {
 	setRotaryListener(distanceChange);
 	setClickListener($("#decreaseDistance"), decreaseDistance);
 	setClickListener($("#increaseDistance"), increaseDistance);
-	/*setClickListener($("#distanceDone"), null);*/
+	setClickListener($("#upArrowDistanceHectometers"), increaseHectometers);
+	setClickListener($("#upArrowDistanceDecameters"), increaseDecameters);
+	setClickListener($("#upArrowDistanceMeters"), increaseMeters);
+	setClickListener($("#hectometers"), selectHectometers);
+	setClickListener($("#decameters"), selectDecameters);
+	setClickListener($("#meters"), selectMeters);
+	setClickListener($("#downArrowDistanceHectometers"), decreaseHectometers);
+	setClickListener($("#downArrowDistanceDecameters"), decreaseDecameters);
+	setClickListener($("#downArrowDistanceMeters"), decreaseMeters);
+	setClickListener($("#distanceDone"), setDistance);
+}
+
+function increaseHectometers() {
+	selectedDistance = DistancesEnum.HECTOMETERS;
+	increaseDistance();
+}
+
+function increaseDecameters() {
+	selectedDistance = DistancesEnum.DECAMETERS;
+	increaseDistance();
+}
+
+function increaseMeters() {
+	selectedDistance = DistancesEnum.METERS;
+	increaseDistance();
+}
+
+function selectHectometers() {
+	selectedDistance = DistancesEnum.HECTOMETERS;
+}
+
+function selectDecameters() {
+	selectedDistance = DistancesEnum.DECAMETERS;
+}
+
+function selectMeters() {
+	selectedDistance = DistancesEnum.METERS;
+}
+
+function decreaseHectometers() {
+	selectedDistance = DistancesEnum.HECTOMETERS;
+	decreaseDistance();
+}
+
+function decreaseDecameters() {
+	selectedDistance = DistancesEnum.DECAMETERS;
+	decreaseDistance();
+}
+
+function decreaseMeters() {
+	selectedDistance = DistancesEnum.METERS;
+	decreaseDistance();
+}
+
+function setDistance() {
+	distance = distances.get(DistancesEnum.HECTOMETERS) + "" + distances.get(DistancesEnum.DECAMETERS) + "" + distances.get(DistancesEnum.METERS);
 }
 
 function distanceChange(ev) {
@@ -35,7 +90,7 @@ function distanceChange(ev) {
 
 function decreaseDistance() {
 	var distance = distances.get(selectedDistance);
-    if (distance > 1) {
+    if (distance > 0) {
         distance--;
         distances.set(selectedDistance, distance);
         $("#"+selectedDistance).text(distances.get(selectedDistance));
