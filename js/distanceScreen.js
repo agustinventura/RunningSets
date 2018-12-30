7edb99ba6c2f5746032235174e8880a18d739667
Agustín Ventura <agustinventura@gmail.com>
2018-12-30 19:42:47 +0100

    adds distance screen format and initial functionality for distances

--- js/distanceScreen.js
+++ js/distanceScreen.js
@@ -1,12 +1,52 @@
+function initDistances() {
+	distances.set(DistancesEnum.METERS, 0);
+	distances.set(DistancesEnum.DECAMETERS, 5);
+	distances.set(DistancesEnum.HECTOMETERS, 0);
+}
+
 function showDistanceScreen() {
 	$("#setsScreen").hide();
 	setDistanceScreenListeners();
+	setDistancesText();
 	$("#distanceScreen").show();
 }
 
+function setDistancesText() {
+	for (var distance in DistancesEnum) {
+		$("#"+DistancesEnum[distance]).text(distances.get(DistancesEnum[distance]));
+	}
+}
+
 function setDistanceScreenListeners() {
-	/*setRotaryListener(distanceChange);
+	setRotaryListener(distanceChange);
 	setClickListener($("#decreaseDistance"), decreaseDistance);
 	setClickListener($("#increaseDistance"), increaseDistance);
-	setClickListener($("#distanceDone"), null);*/
+	/*setClickListener($("#distanceDone"), null);*/
+}
+
+function distanceChange(ev) {
+	var direction = ev.detail.direction;
+    if (direction === "CW") {
+        increaseDistance();
+    } else {
+        decreaseDistance();
+    }
+}
+
+function decreaseDistance() {
+	var distance = distances.get(selectedDistance);
+    if (distance > 1) {
+        distance--;
+        distances.set(selectedDistance, distance);
+        $("#"+selectedDistance).text(distances.get(selectedDistance));
+    }
+}
+
+function increaseDistance() {
+	var distance = distances.get(selectedDistance);
+    if (distance < 9) {
+        distance++;
+        distances.set(selectedDistance, distance);
+        $("#"+selectedDistance).text(distances.get(selectedDistance));
+    }
 }
--- js/runningSets.js
+++ js/runningSets.js
@@ -1,7 +1,17 @@
+DistancesEnum = {
+	METERS: "meters",
+	DECAMETERS: "decameters",
+	HECTOMETERS: "hectometers"
+}
+
 var sets = 6;
+var distances = new Map();
+var distance = 50;
+var selectedDistance = DistancesEnum.DECAMETERS; 
 
 function init() {
 	setBackKeyListener();
+	initDistances();
 	showSetsScreen();
 }
 
