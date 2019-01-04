92b187b89ec3dfb2af647eecbcd2f8697ab86d13
Agustín Ventura <agustinventura@gmail.com>
2019-01-04 13:16:48 +0100

    adds sum up data to end screen

--- js/endScreen.js
+++ js/endScreen.js
@@ -1,9 +1,21 @@
 function showEndScreen() {
 	hideAllScreens();
 	setEndScreenListeners();
+	setEndScreenTexts();
 	$("#endScreen").show();
 }
 
 function setEndScreenListeners() {
 	setRotaryListener(null);
+}
+
+function setEndScreenTexts() {
+	$("#finishedSets").text(currentSet);
+	$("#totalDistance").text(currentSet * distance);
+	$("#averageSetTime").text((training.totalSetsTime.getTime() / 1000) / currentSet);
+	if (currentSet > 1) {
+		$("#averageRestTime").text((training.totalRestTime.getTime() / 1000) / (currentSet - 1));
+	} else {
+		$("#averageRestTime").text("0");
+	}
 }
