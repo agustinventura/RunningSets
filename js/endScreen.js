1683d9265f7f7db32fc37b383d939dc75c105dce
Agustín Ventura <agustinventura@gmail.com>
2019-01-06 12:23:48 +0100

    adds format to end screen averages

--- js/endScreen.js
+++ js/endScreen.js
@@ -12,9 +12,9 @@ function setEndScreenListeners() {
 function setEndScreenTexts() {
 	$("#finishedSets").text(currentSet);
 	$("#totalDistance").text(currentSet * distance);
-	$("#averageSetTime").text((training.totalSetsTime.getTime() / 1000) / currentSet);
+	$("#averageSetTime").text(((training.totalSetsTime.getTime() / 1000) / currentSet).toFixed(2));
 	if (currentSet > 1) {
-		$("#averageRestTime").text((training.totalRestTime.getTime() / 1000) / (currentSet - 1));
+		$("#averageRestTime").text(((training.totalRestTime.getTime() / 1000) / (currentSet - 1)).toFixed(2));
 	} else {
 		$("#averageRestTime").text("0");
 	}
