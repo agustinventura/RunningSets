d0536e8fa9656b58d168512ecc3eae2c6b7531f7
Agustín Ventura <agustinventura@gmail.com>
2019-01-18 18:56:38 +0100

    fixes rest time representation in end screen

--- js/endScreen.js
+++ js/endScreen.js
@@ -14,7 +14,7 @@ function setEndScreenTexts() {
 	$("#totalDistance").text(currentSet * distance);
 	$("#averageSetTime").text(((training.totalSetsTime.getTime() / 1000) / currentSet).toFixed(2));
 	if (currentSet > 1) {
-		$("#averageRestTime").text(((training.totalRestTime.getTime() / 1000) / (currentSet - 1)).toFixed(2));
+		$("#averageRestTime").text((restTime - ((training.totalRestTime.getTime() / 1000) / (currentSet - 1))).toFixed(2));
 	} else {
 		$("#averageRestTime").text("0");
 	}
